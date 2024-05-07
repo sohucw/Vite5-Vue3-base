9ad87a805d6966cd0b803deead4a292b86bb30d0
dawei <chenjianwei01@baidu.com>
2024-05-07 10:40:45 +0800

    feat: init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+// import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <div class="home">
+    <h1>This is an home page</h1>
+  </div>
+</template>
+
+<style>
+</style>
