4dac8f45ceef13916d9a54801a511def2dd8a314
sohucw <sohucw@users.noreply.github.com>
2024-05-07 07:20:05 +0000

    deploy: 680bb39629a938ec66284bf8eca528cc11a9cdec

--- assets/DemoView-D0c-itX9.js
+++
@@ -1,2 +0,0 @@
-import{d as o,o as n,c as a,a as l}from"./index-BA6aMcxS.js";const u=o({__name:"DemoView",setup(s){let e=1;const t=()=>{e=null,e.length===1&&console.log("出错了")};return(c,r)=>(n(),a("div",null,[l("button",{onClick:t},"测试")]))}});export{u as default};
-//# sourceMappingURL=DemoView-D0c-itX9.js.map
