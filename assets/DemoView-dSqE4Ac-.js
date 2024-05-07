16e77e4edd4ee2aac1173e2a4bd310d94809ef63
sohucw <sohucw@users.noreply.github.com>
2024-05-07 08:39:39 +0000

    deploy: 57457bd7e4dde3834237ded699d9ad65635f5b58

---
+++ assets/DemoView-dSqE4Ac-.js
@@ -0,0 +1,2 @@
+import{d as o,o as n,c as a,a as l}from"./index-C_jJfI7v.js";const u=o({__name:"DemoView",setup(s){let e=1;const t=()=>{e=null,e.length===1&&console.log("出错了")};return(c,r)=>(n(),a("div",null,[l("button",{onClick:t},"测试")]))}});export{u as default};
+//# sourceMappingURL=DemoView-dSqE4Ac-.js.map
